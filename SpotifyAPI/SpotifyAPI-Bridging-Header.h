9072bb9ca24f9339c47691e9c9a1bd35a664b72f
Raphael Tarnoczi <raphi.tarnoczi@gmail.com>
2025-09-06 13:03:23 +0200

    first setup for connection to spotify api

---
+++ SpotifyAPI/SpotifyAPI-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  SpotifyAPI-Bridging-Header.h
+//  SpotifyAPI
+//
+//  Created by Raphael Tarnoczi on 06.09.25.
+//
+
+#ifndef SpotifyAPI_Bridging_Header_h
+#define SpotifyAPI_Bridging_Header_h
+
+#import <SpotifyiOS/SpotifyiOS.h>
+
+#endif /* SpotifyAPI_Bridging_Header_h */
