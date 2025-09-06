bc6d0594c0f40db4cdc44549b1302c6932d954b8
Raphael Tarnoczi <raphi.tarnoczi@gmail.com>
2025-09-06 12:40:39 +0200

    Initial Commit

---
+++ SpotifyAPI/SpotifyAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyAPIApp.swift
+//  SpotifyAPI
+//
+//  Created by Raphael Tarnoczi on 06.09.25.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
